666df41ed5de90431465471c5745663956c081b2
Gotoligcol <crustacemer50@hotmail.fr>
2015-11-22 19:07:22 +0100

    Update level_editor.c

--- level_editor.c
+++ level_editor.c
@@ -11,14 +11,14 @@
 
 int sauvegarde_niveau(int valeurs[15][19], char mot[50])
 {
-    int nb,i=0,j=0;
+    int nb,i = 0,j = 0;
     FILE* noms_sauvegardes;
     FILE* sauvegarde;
-    noms_sauvegardes= fopen("noms_sauvegardes.txt", "r+t");
-    fscanf(noms_sauvegardes, "%d", &nb);
+    noms_sauvegardes= fopen("noms_sauvegardes.txt", "r+t");//ouvre le fichier pour stocker les noms des saves
+    fscanf(noms_sauvegardes, "%d", &nb);//nbre de niveaux pre enregistres
     if (strlen(mot)<44)
     {
-    strcat(mot, ".txt");
+    strcat(mot, ".txt");//save dun fichier texte pour gerer le niveau creer 
     }
     else
     {
@@ -27,7 +27,7 @@ int sauvegarde_niveau(int valeurs[15][19], char mot[50])
     }
     sauvegarde = fopen(mot, "w+");
 
-    fseek(noms_sauvegardes, 0, SEEK_SET);
+    fseek(noms_sauvegardes, 0, SEEK_SET);// on change le nbre de niveau deja pre enregistre uniquement par le lvl editor
     fprintf(noms_sauvegardes, "%d ", ++nb);
     fseek(noms_sauvegardes, 0, SEEK_END);
     fprintf(noms_sauvegardes, " %s", mot);
@@ -37,7 +37,7 @@ int sauvegarde_niveau(int valeurs[15][19], char mot[50])
     {
         for(j=0; j<19; j++)
                 {
-                    fprintf(sauvegarde, "%d ", valeurs[i][j]);
+                    fprintf(sauvegarde, "%d ", valeurs[i][j]);// on creer le fichier correspondant au niveau qui vient detre creer 
                 }
     }
     fclose(sauvegarde);
@@ -46,7 +46,7 @@ int sauvegarde_niveau(int valeurs[15][19], char mot[50])
 }
 int conversion(int num, int *compteur)
 {
-    switch (num)
+    switch (num) // associe au tableau de valeurs les valeurs de la case vue a lecran quand on edite
     {
     case 0:
         num= 2;
@@ -88,9 +88,9 @@ int conversion(int num, int *compteur)
     case 12:
         num=8;
         break;
-    case 13:
+    case 13:// teleporteur
         printf("%d", *compteur);
-        num=101+((*compteur)%2)*10+(*compteur)-(*compteur)%2;
+        num=101+((*compteur)%2)*10+(*compteur)-(*compteur)%2;)//gere que 1 fois sur 2, le tp vaut la valeur du tp precedent+10 
         (*compteur)++;
         break;
     default:
@@ -137,51 +137,51 @@ clear_keybuf();
 return compteur;
 }
 
-void editeur_de_case(BITMAP* affiche[15][19], int valeurs[15][19])
+void editeur_de_case(BITMAP* affiche[15][19], int valeurs[15][19]) //edite les cases: quand on clique change le motif de la case
 {
     int compteur=0;
     int Case=0;
     int test=1,x=100,y=100;
     BITMAP* buffer;
     BITMAP* affiche2[NBSPRITE];
-    init_mode_graphique(1, affiche2);
-    buffer= create_bitmap(TSPRITE*19, TSPRITE*15);
+    init_mode_graphique(1, affiche2);// prend le tableau de beatmap affiche 2 et charge tourtes les images pour editer
+    buffer= create_bitmap(TSPRITE*19, TSPRITE*15);// buffer qui fais la taille de lecran
     buffer= affiche_buffer(affiche);
-    show_mouse(buffer);
-    blit(buffer, screen ,0,0,0,0, 19*TSPRITE, 15*TSPRITE);
+    show_mouse(buffer);/:affiche la souris sur limage buffer
+    blit(buffer, screen ,0,0,0,0, 19*TSPRITE, 15*TSPRITE);//on imprime tout ca sur lecran
     rest(500);
     while (test)
     {
-        poll_mouse();
-        show_mouse(buffer);
-        blit(buffer, screen ,0,0,0,0, 19*TSPRITE, 15*TSPRITE);
+        poll_mouse();//securite pour update les coordonnees de la souris
+        show_mouse(buffer);//affiche la souris sur le buffer
+        blit(buffer, screen ,0,0,0,0, 19*TSPRITE, 15*TSPRITE);//affiche le buffer tant que lon ne clique pas 
 
-    if ((mouse_b & 1))
+    if ((mouse_b & 1))//mouse_b = position de la souris, &1 = indique si jamais lutilisateur clique sur le bouton de gauche 
     {
-        if (((int)(mouse_x-mouse_x%TSPRITE)/TSPRITE!=x)||((int)(mouse_y-mouse_y%TSPRITE)/TSPRITE!=y))
+        if (((int)(mouse_x-mouse_x%TSPRITE)/TSPRITE!=x)||((int)(mouse_y-mouse_y%TSPRITE)/TSPRITE!=y))// si on est sur une case qui nest pas la meme que la precedente 
         {
         Case=0;
-        x= (int)(mouse_x-mouse_x%TSPRITE)/TSPRITE;
+        x= (int)(mouse_x-mouse_x%TSPRITE)/TSPRITE;//on met x et y aux valeurs de la case sur laquelle on vient de cliquer
         y=(int)(mouse_y-mouse_y%TSPRITE)/TSPRITE;
-        affiche[(int)(mouse_y-mouse_y%TSPRITE)/TSPRITE][(int)(mouse_x-mouse_x%TSPRITE)/TSPRITE]=affiche2[Case];
+        affiche[(int)(mouse_y-mouse_y%TSPRITE)/TSPRITE][(int)(mouse_x-mouse_x%TSPRITE)/TSPRITE]=affiche2[Case];// change la case sur laquelle on vient de cliquer a limage de la premiere case de affiche 2
         valeurs[(int)(mouse_y-mouse_y%TSPRITE)/TSPRITE][(int)(mouse_x-mouse_x%TSPRITE)/TSPRITE]= conversion(Case, &compteur);
         buffer= create_bitmap(TSPRITE*19, TSPRITE*15);
         buffer= affiche_buffer(affiche);
         show_mouse(buffer);
-        blit(buffer, screen ,0,0,0,0, 19*TSPRITE, 15*TSPRITE);
-        rest(100);
-        destroy_bitmap(buffer);
+        blit(buffer, screen ,0,0,0,0, 19*TSPRITE, 15*TSPRITE);//copie le buffer
+        rest(100);//car blit prend du temps avant de detruire le buffer
+        destroy_bitmap(buffer);//detruit le buffer: on nettoie sa place memoire pour eviter des problemes
         }
-        else
+        else//gere si jamais on a changer de case pour recommencer la boucle a zero pour eviter dafficher par ex une echelle sur la case, puis changer de case et que saffiche un tp (la valeur apres lechelle)
         {
             if (Case==13)
             {
                 Case=-1;
                 compteur--;
             }
-        affiche[(int)(mouse_y-mouse_y%TSPRITE)/TSPRITE][(int)(mouse_x-mouse_x%TSPRITE)/TSPRITE]=affiche2[++Case];
-        valeurs[(int)(mouse_y-mouse_y%TSPRITE)/TSPRITE][(int)(mouse_x-mouse_x%TSPRITE)/TSPRITE]= conversion(Case, &compteur);
-        buffer= create_bitmap(TSPRITE*19, TSPRITE*15);
+        affiche[(int)(mouse_y-mouse_y%TSPRITE)/TSPRITE][(int)(mouse_x-mouse_x%TSPRITE)/TSPRITE]=affiche2[++Case];//incremente la case pour afficher pour afficher limage suivante
+        valeurs[(int)(mouse_y-mouse_y%TSPRITE)/TSPRITE][(int)(mouse_x-mouse_x%TSPRITE)/TSPRITE]= conversion(Case, &compteur);//gere la valeur en dure de limage affichee par la ligne precedente
+        buffer= create_bitmap(TSPRITE*19, TSPRITE*15);// creer emplacement memoire pour limage qui fais la taille de lecran
         buffer= affiche_buffer(affiche);
         show_mouse(buffer);
         blit(buffer, screen ,0,0,0,0, 19*TSPRITE, 15*TSPRITE);
@@ -190,19 +190,19 @@ void editeur_de_case(BITMAP* affiche[15][19], int valeurs[15][19])
         }
 
     }
-    if (key[KEY_ENTER])
+    if (key[KEY_ENTER])//si lutilisateur appuie sur entree
     {
-        if (compteur%2==0)
+        if (compteur%2==1)//si compteur est impair, affiche le pb pour les tp
         {
             allegro_message("attention!\n les telporteurs sont liées 2 à 2! il ne peut pas y en avoir un nombre impair!");
         }
         else test--;
     }
 
-    if(mouse_b & 2)
+    if(mouse_b & 2)// 
     {
-        if (Case==9) compteur--;
-        Case=7;
+        if (Case==13) compteur--;//si la case quon quitte ets un tp, on decremente le compteur
+        Case=7;//case de glace si clique droit
         x= (int)(mouse_x-mouse_x%TSPRITE)/TSPRITE;
         y=(int)(mouse_y-mouse_y%TSPRITE)/TSPRITE;
         affiche[(int)(mouse_y-mouse_y%TSPRITE)/TSPRITE][(int)(mouse_x-mouse_x%TSPRITE)/TSPRITE]=affiche2[Case];
@@ -220,7 +220,7 @@ void editeur_de_case(BITMAP* affiche[15][19], int valeurs[15][19])
 void worst_writing_ever(char mot[50], BITMAP* buffer, BITMAP* ecran_noir, int line, BITMAP* affiche[15][19]){
 
    int key_allegro;
-   int memoire_char=0;
+   int memoire_char=0;//taille du mot jusqua maintenant 
    char key_ascii;
    char scancode;
    int i=0;
@@ -230,35 +230,33 @@ void worst_writing_ever(char mot[50], BITMAP* buffer, BITMAP* ecran_noir, int li
     while(!key[KEY_ENTER])
     {
         key_allegro=readkey();
-        key_ascii= key_allegro & 0xff;
+        key_ascii= key_allegro & 0xff;//donne le charactere ascii de la touche qui a ete appuye
         scancode= key_allegro >> 8;
 
-    if ((scancode==KEY_BACKSPACE)&&(i!=0))
+    if ((key_allegro==KEY_BACKSPACE)&&(i!=0))
     {
         i--;
 
     }
-    if((key_ascii >= 32) && (key_ascii <= 126))
+    if((key_ascii >= 32) && (key_ascii <= 126))//si ça fait partie de la table ascii
     {
         if (i<49)
         {
-            mot[i]= key_ascii;
+            mot[i]= key_ascii;//on prend la lettre dans le mot que l'utilisateur a ecrit, puis on laffiche 
             textprintf_ex(ecran_noir, font, memoire_char, line*LIGNE+5, makecol(255, 255, 255),-1, "%c", mot[i]);
-            memoire_char+= text_length( font, "c");
-            destroy_bitmap(buffer);
+            memoire_char+= text_length( font, "c");//on lui rajoute la largeur dun caractere, du coup la prochaine fois il saffichera 1 largeur de caractere plus loin
+            destroy_bitmap(buffer);//detruit le buffer
             buffer=affiche_buffer(affiche);
-            draw_sprite(buffer, ecran_noir, 192-2*32, 160);
-            blit(buffer, screen,0,0,0,0, 19*TSPRITE, 15*TSPRITE);
-            mot[i+1]='\0';
+            draw_sprite(buffer, ecran_noir, 192-2*32, 160);//on a recreer un buffer avec des murs partout on a remis le menu de ledito + le mot quon est e train decrire
+            blit(buffer, screen,0,0,0,0, 19*TSPRITE, 15*TSPRITE);//nous montre ce quon est en train de faire
+            mot[i+1]='\0';//on sassure que la chaine sarrete
             i++;
         }
     }
     }
-
-mot[i]='\0';
 }
 
-BITMAP* affiche_buffer(BITMAP* affiche[15][19])
+BITMAP* affiche_buffer(BITMAP* affiche[15][19])// elle prend un buffer et elle met des murs partout 
 {
     BITMAP* buffer;
     buffer= create_bitmap(TSPRITE*19, TSPRITE*15);
@@ -285,9 +283,9 @@ void level_editor()
     install_keyboard();
     install_mouse();
     set_color_depth(16);
-    enable_hardware_cursor();
-    BITMAP* affiche[15][19]={{load_bitmap("mur.bmp", NULL)}};
-    ecran_noir= create_bitmap(192+5*32, 160);
+    enable_hardware_cursor();//prend l'image du curseur de base
+    BITMAP* affiche[15][19]={{load_bitmap("mur.bmp", NULL)}};//un foooooooond de murs
+    ecran_noir= create_bitmap(192+5*32, 160);//crer un ecran noir
     clear_to_color(ecran_noir, makecol(255, 255, 0));
     buffer= create_bitmap(TSPRITE*19, TSPRITE*15);
     if(set_gfx_mode(GFX_AUTODETECT_WINDOWED, TSPRITE*19, TSPRITE*15, 0, 0)==0)
@@ -297,45 +295,43 @@ void level_editor()
     {
         for(j=0; j<19; j++){
                 affiche[i][j]=affiche[0][0];
-                if (affiche[i][j]!=NULL) draw_sprite(buffer, affiche[i][j], TSPRITE*j, TSPRITE*i);
+                if (affiche[i][j]!=NULL) draw_sprite(buffer, affiche[i][j], TSPRITE*j, TSPRITE*i); //beatmap constitué de murs
         }
     }
     textprintf_ex(ecran_noir, font, 0, LIGNE, makecol(255,255,255),-1, "bienvenue sur l'editeur de niveau");
     draw_sprite(buffer, ecran_noir, 192-2*32, 160);
-    blit(buffer, screen,0,0,0,0, 19*TSPRITE, 15*TSPRITE);
+    blit(buffer, screen,0,0,0,0, 19*TSPRITE, 15*TSPRITE);//Ici, on a une image rempli de murs, et par dessus une autre image avec le mess de bvn
     rest(1100);
-    menu(buffer, affiche, mot);
+    menu(buffer, affiche, mot);//fonction menue de l editeur
     destroy_bitmap(buffer);
     }
 }
-void menu(BITMAP* buffer, BITMAP* affiche[15][19], char mot[50]){
+void menu(BITMAP* buffer, BITMAP* affiche[15][19], char mot[50]){//buffer=ecran noir afficher
     int x=1,i,j;
     BITMAP* ecran_noir;
     BITMAP* sauvegarde[15][19];
     BITMAP* ecran_selec;
     int valeurs[15][19];
     int line;
-    ecran_noir= create_bitmap(192+5*32, 160);
+    ecran_noir= create_bitmap(192+5*32, 160);//on refait un ecran noir
     ecran_selec= create_bitmap(192+5*32, 160);
-    clear_to_color(ecran_noir, makecol(0, 0, 0));
+    clear_to_color(ecran_noir, makecol(0, 0, 0));//on colorie bien tout en noir
     textprintf_ex(ecran_noir, font, 0, LIGNE, makecol(255,255,255),-1, "Que souhaitez-vous faire?");
     textprintf_ex(ecran_noir, font, 0, 2*LIGNE+3, makecol(255,255,255),-1, "'dessin' pour commencer a dessiner");
     textprintf_ex(ecran_noir, font, 0, 3*LIGNE+3, makecol(255,255,255),-1, "'aide' pour voir l'aide au dessin!important!");
-    blit(ecran_noir, ecran_selec, 0,0,0,0, 192+5*32, 160 );
-    line=4;
+    blit(ecran_noir, ecran_selec, 0,0,0,0, 192+5*32, 160 );//on a ecrit sur lecran noir pour faire le menu
+    line=4;//compteur de ligne
      affiche[0][0]=load_bitmap("mur.bmp", NULL);
     for (i=0; i<15; i++)
     {
         for(j=0; j<19; j++){
-                valeurs[i][j]=-2;
-                printf("%d",valeurs[i][j]);
-                affiche[i][j]=affiche[0][0];
+                valeurs[i][j]=-2;//murs
+                affiche[i][j]=affiche[0][0];//tout est initialise a murs, du coup lecran noir disparait
                 if (affiche[i][j]!=NULL) draw_sprite(buffer, affiche[i][j], TSPRITE*j, TSPRITE*i);
         }
-        printf("\n");
     }
      draw_sprite(buffer, ecran_noir, 192-2*32, 160);
-    blit(buffer, screen,0,0,0,0, 21*TSPRITE, 15*TSPRITE);
+    blit(buffer, screen,0,0,0,0, 21*TSPRITE, 15*TSPRITE);//on a notre fond avec des fonds et un ecran noir avec le menu
     while(x)
     {
     i=0;
@@ -345,17 +341,17 @@ void menu(BITMAP* buffer, BITMAP* affiche[15][19], char mot[50]){
         for(j=0; j<19; j++)
             {
 
-                sauvegarde[i][j]=affiche[i][j];
+                sauvegarde[i][j]=affiche[i][j];//tableau de sauvegarde de affiche [i][j]
             }
     }
             worst_writing_ever(mot, buffer, ecran_noir, line, affiche);
-            if (strcmp(mot, "dessin"))
+            if (strcmp(mot, "dessin"))//compare le mot quon vient de rentrer a dessin et aide
             {
                 if (strcmp(mot, "aide"))
                 {
-               allegro_message("ce n'est pas une entree valable");
+               allegro_message("ce n'est pas une entree valide!");//pop up blindage
                 }
-                else
+                else// affiche laide pour editer
                 {
                     clear_to_color(ecran_noir, makecol(0,0,0));
                     textprintf_ex(ecran_noir, font, 0, LIGNE+3, makecol(255,255,255),-1, "Afin de dessiner un niveau cliquez sur les cases.");
@@ -368,39 +364,37 @@ void menu(BITMAP* buffer, BITMAP* affiche[15][19], char mot[50]){
                     textprintf_ex(ecran_noir, font, 0, 8*(LIGNE+3), makecol(255,255,255),-1, "Soyez créatifs!!!");
                 }
             }
-            else
+            else//fonction pour editer
             {
                 editeur_de_case(affiche, valeurs);
-                buffer=affiche_buffer(affiche);
-                draw_sprite(buffer, ecran_noir, 192-2*32, 160);
-                blit(buffer, screen,0,0,0,0, 21*TSPRITE, 15*TSPRITE);
-                x=menu_oui_non(buffer, "dessin");
+                buffer=affiche_buffer(affiche);//nouveau tableau daffichage pour le snouvelles bonnes valeurs daffiche
+                x=menu_oui_non(buffer, "dessin");//affiche niveau en fd decran et oui ou non pour save
                 line++;
-                if(x==0)
+                if(x==0)//on save
                 {
                     clear_to_color(ecran_noir, makecol(0,0,0));
-                    textprintf_ex(ecran_noir, font, 0, 0, makecol(255,255,255),-1, "entrez le nom de votre niveau");
+                    textprintf_ex(ecran_noir, font, 0, 0, makecol(255,255,255),-1, "entrez le nom de votre niveau");//sauvegarde du lvl
                     worst_writing_ever(mot, buffer, ecran_noir, 1, affiche);
-                    if (sauvegarde_niveau(valeurs, mot));
+                    if (sauvegarde_niveau(valeurs, mot));//si il a return 1, on repropose ce que lutilisateur veut faire
                     else
                     {
-                        allegro_message("niveau enregistre!");
+                        allegro_message("niveau enregistre!");// si sauvegarde niveau return 0, le niveau est enregistre
                         x=0;
                     }
                 }
-                else
+                else// si lutilisateur a dit non pour save le niveau
                 {
 
                         for (i=0; i<15; i++)
                             {
                                 for(j=0; j<19; j++)
                                     {
-                                        affiche[i][j]=sauvegarde[i][j];
+                                        affiche[i][j]=sauvegarde[i][j];// affiche le dernier etat du niveau avant quil commence a dessiner
                                     }
                             }
                 }
             }
-            blit(ecran_selec, ecran_noir, 0,0,0,0, 192+5*32, 160 );
+            blit(ecran_selec, ecran_noir, 0,0,0,0, 192+5*32, 160 );// des que lon a fini, re affiche le menu avec possibilite de dessiner ou de daffichzer laide
         }
     }
 
